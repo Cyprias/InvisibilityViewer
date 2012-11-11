d481bb2514467ec871cf01d9cf34007e07fbb864
Cyprias <Cyprias@gmail.com>
2012-11-11 13:47:12 -0700

    * Added event handler for our version checker.

--- src/com/cyprias/invisibilityviewer/Config.java
+++ src/com/cyprias/invisibilityviewer/Config.java
@@ -1,11 +1,6 @@
 package com.cyprias.invisibilityviewer;
 
-import java.util.HashMap;
-import java.util.List;
-
 import org.bukkit.configuration.Configuration;
-import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.configuration.file.FileConfiguration;
 
 
 public class Config {
---
+++ src/com/cyprias/invisibilityviewer/Events.java
@@ -0,0 +1,38 @@
+package com.cyprias.invisibilityviewer;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import com.cyprias.invisibilityviewer.VersionChecker.VersionCheckerEvent;
+
+public class Events implements Listener {
+	private InvisibilityViewer plugin;
+
+	public Events(InvisibilityViewer plugin) {
+		this.plugin = plugin;
+	}
+
+	
+
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void onVersionCheckerEvent(VersionCheckerEvent event) {
+
+		if (event.getPluginName() == plugin.getName()) {
+			VersionChecker.versionInfo info = event.getVersionInfo(0);
+			Object[] args = event.getArgs();
+
+			String curVersion = plugin.getDescription().getVersion();
+
+			if (args.length == 0) {
+
+				int compare = plugin.versionChecker.compareVersions(curVersion, info.getTitle());
+				if (compare < 0) {
+					plugin.info("We're running v" + curVersion + ", v" + info.getTitle() + " is available");
+					plugin.info(info.getLink());
+				}
+
+				return;
+			}
+		}
+	}
+}
