e2026d5a97518cca77e488e9c94aa2756029a9c9
Cyprias <Cyprias@gmail.com>
2012-11-11 13:14:29 -0700

    * First commit.

---
+++ src/com/cyprias/invisibilityviewer/InvisibilityViewer.java
@@ -0,0 +1,172 @@
+package com.cyprias.invisibilityviewer;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import net.minecraft.server.MobEffectList;
+import net.minecraft.server.WatchableObject;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import com.comphenix.protocol.Packets;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.ConnectionSide;
+import com.comphenix.protocol.events.ListenerPriority;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.events.PacketEvent;
+import com.comphenix.protocol.reflect.FieldAccessException;
+import com.comphenix.protocol.reflect.StructureModifier;
+
+public class InvisibilityViewer extends JavaPlugin {
+	public static String chatPrefix = "§f[§aIV§f] ";
+
+	private ProtocolManager protocolManager;
+	private String stPluginEnabled = "§f%s §7v§f%s §7is enabled.";
+	String pluginName;
+
+	private Entity getEntity(List<Entity> ents, int eID) {
+		for (int i = 0; i < ents.size(); i++) {
+			if (ents.get(i).getEntityId() == eID)
+				return ents.get(i);
+		}
+
+		return null;
+	}
+	public VersionChecker versionChecker;
+	public void onEnable() {
+		pluginName = getDescription().getName();
+
+		this.versionChecker = new VersionChecker(this, "http://dev.bukkit.org/server-mods/invisibilityviewer/files.rss");
+		try {
+			Metrics metrics = new Metrics(this);
+			metrics.start();
+		} catch (IOException e) {
+		}
+		
+		setupPacketHandler();
+
+		 info(String.format(stPluginEnabled, pluginName,this.getDescription().getVersion()));
+	}
+
+	private void setupPacketHandler(){
+		
+		protocolManager = ProtocolLibrary.getProtocolManager();
+		protocolManager.addPacketListener(new PacketAdapter(this, ConnectionSide.SERVER_SIDE, ListenerPriority.NORMAL, 0x28) {
+			public void onPacketSending(PacketEvent event) {
+				// Item packets
+				PacketContainer packet = event.getPacket();
+
+				Player player = event.getPlayer();
+
+				switch (event.getPacketID()) {
+				case Packets.Server.ENTITY_METADATA: // Entity Metadata
+
+					StructureModifier<Object> mods = packet.getModifier();
+
+					WatchableObject changedData;
+					Entity entity = null;
+					if (mods.size() > 0) {
+						int eID = 0;
+						try {
+							eID = (Integer) mods.read(0);
+						} catch (FieldAccessException e) {
+							e.printStackTrace();
+						}
+						entity = getEntity(player.getWorld().getEntities(), eID);
+
+						for (int i = 1; i < mods.size(); i++) {
+							try {
+								if (mods.read(i) instanceof ArrayList) {
+									ArrayList<WatchableObject> list = (ArrayList<WatchableObject>) mods.read(i);
+
+									for (int a = 0; a < list.size(); a++) {
+
+										if (list.get(a) instanceof WatchableObject) {
+											changedData = removeInvisibility((WatchableObject) list.get(a));
+
+											if (changedData != null) {
+												if (entity instanceof Player) {
+													
+													if (hasPermission(player, "invisibilityviewer.player")){
+														list.set(a, changedData);
+														mods.write(i, list);
+													}
+													
+												}else {
+													if (hasPermission(player, "invisibilityviewer.other")){
+														list.set(a, changedData);
+														mods.write(i, list);
+													}
+												}
+											}
+										}
+
+									}
+
+								}
+							} catch (FieldAccessException e) {
+								e.printStackTrace();
+							}
+						}
+					}
+
+					break;
+				}
+			}
+		});
+		
+	}
+	
+	public WatchableObject removeInvisibility(WatchableObject data) {
+		switch (data.a()) {
+		case 0:// Flags
+			try {
+				Byte b = (Byte) data.b();
+
+				if (b == 32) {
+					return new WatchableObject(data.c(), data.a(), (byte) 0);
+				}
+
+			} catch (NumberFormatException e) {
+			}
+			break;
+		}
+		return null;
+	}
+
+	public void info(String msg) {
+		getServer().getConsoleSender().sendMessage(chatPrefix + msg);
+	}
+
+	public boolean hasPermission(CommandSender sender, String node) {
+		if (!(sender instanceof Player)) {
+			return true;
+		}
+		Player player = (Player) sender;
+
+		if (player.isPermissionSet(node)) // in case admin purposely set the
+											// node to false.
+			return player.hasPermission(node);
+
+		if (player.isPermissionSet(pluginName.toLowerCase() + ".*"))
+			return player.hasPermission(pluginName.toLowerCase() + ".*");
+
+		String[] temp = node.split("\\.");
+		String wildNode = temp[0];
+		for (int i = 1; i < (temp.length); i++) {
+			wildNode = wildNode + "." + temp[i];
+
+			if (player.isPermissionSet(wildNode + ".*"))
+				// plugin.info("wildNode1 " + wildNode+".*");
+				return player.hasPermission(wildNode + ".*");
+		}
+
+		return player.hasPermission(node);
+	}
+}
---
+++ src/com/cyprias/invisibilityviewer/VersionChecker.java
@@ -0,0 +1,185 @@
+package com.cyprias.invisibilityviewer;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+public class VersionChecker {
+	private String curseRSS;
+	private JavaPlugin plugin;
+	//private Logger log = Logger.getLogger("Minecraft");
+
+	private String pluginName;
+
+	public VersionChecker(JavaPlugin plugin, String curseRSS) {
+		this.plugin = plugin;
+		this.curseRSS = curseRSS;
+		this.pluginName = plugin.getName();
+	}
+
+	public void retreiveVersionInfo(Object... args) {
+		getVersionInfoTask task = new getVersionInfoTask(this);
+		int taskID = plugin.getServer().getScheduler().scheduleAsyncDelayedTask(plugin, task, 0L);
+		task.setId(taskID);
+		task.setArgs(args);
+	}
+
+	public static int compareVersions(String a, String b){
+		String[] aParts = a.split("\\.");
+		String[] bParts = b.split("\\.");
+
+		int aInt, bInt;
+		
+		int i=0;
+		while (aParts.length > i && bParts.length > i) {
+			aInt = Integer.parseInt(aParts[i]);
+			bInt = Integer.parseInt(bParts[i]);
+			if (aInt != bInt){
+				return aInt - bInt;
+			}
+			i+=1;
+		}
+		return 0;
+	}
+	
+	List<versionInfo> versions = new ArrayList<versionInfo>();
+
+	public static class versionInfo {
+		private String title;
+		private String link;
+		private String description[];
+		private String pubDate;
+
+		public versionInfo(String title2, String link2, String pubDate2, String description2) {
+			this.title = title2;
+			this.link = link2;
+			this.pubDate = pubDate2;
+			description2 = description2.replaceAll("\\<.*?>", "");
+			this.description = description2.split("\\r?\\n");
+		}
+
+		public String getTitle() {
+			return this.title;
+		}
+
+		public String getLink() {
+			return this.link;
+		}
+
+		public String getPubDate() {
+			return this.pubDate;
+		}
+
+		public String[] getDescription() {
+			return this.description;
+		}
+	}
+
+	private class getVersionInfoTask implements Runnable {
+		private VersionChecker me;
+		private Object[] args;
+
+
+		public getVersionInfoTask(VersionChecker me2) {
+			this.me = me2;
+		}
+		public void setArgs(Object... args) {
+			this.args = args;
+		}
+
+		
+		private int taskID;
+		public void setId(int n) {
+			this.taskID = n;
+		}
+
+		@Override
+		public void run() {
+			try {
+				URL url = new URL(me.curseRSS);
+				Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(url.openConnection().getInputStream());
+				doc.getDocumentElement().normalize();
+				NodeList nodes = doc.getElementsByTagName("item");
+
+				Element element, subElement;
+				NodeList subNodes;
+
+				//log.info(ChatColor.WHITE + "getVersionInfoTask run B getLenth: " + nodes.getLength());
+
+				if (nodes.getLength() > 0) {
+					versions.clear();
+
+					String title, link, pubDate, description;
+					for (int v = 0; v < nodes.getLength(); v++) {
+
+						element = (Element) nodes.item(v);
+						subNodes = element.getElementsByTagName("title");
+						subElement = (Element) subNodes.item(0);
+						title = subElement.getChildNodes().item(0).getNodeValue();
+
+						subNodes = element.getElementsByTagName("link");
+						subElement = (Element) subNodes.item(0);
+						link = subElement.getChildNodes().item(0).getNodeValue();
+
+						subNodes = element.getElementsByTagName("pubDate");
+						subElement = (Element) subNodes.item(0);
+						pubDate = subElement.getChildNodes().item(0).getNodeValue();
+
+						subNodes = element.getElementsByTagName("description");
+						subElement = (Element) subNodes.item(0);
+						description = subElement.getChildNodes().item(0).getNodeValue();
+						versions.add(new versionInfo(title, link, pubDate, description));
+					}
+				}
+
+			} catch (Exception localException) {
+			}
+			VersionCheckerEvent event = new VersionCheckerEvent(me.plugin.getName(), versions, this.args);
+			me.plugin.getServer().getPluginManager().callEvent(event);
+		}
+	}
+
+	public static class VersionCheckerEvent extends Event {
+		private static final HandlerList handlers = new HandlerList(); 
+		public HandlerList getHandlers() { return handlers; }
+		public static HandlerList getHandlerList() { return handlers; }
+		 
+		private List<versionInfo> versions;
+
+		private String pluginName;
+		private Object[] args;
+		public VersionCheckerEvent(String pluginName, List<versionInfo> versions, Object... args) {
+			this.pluginName = pluginName;
+			this.versions = versions;
+			this.args = args;
+		}
+
+		public String getPluginName() {
+			return this.pluginName;
+		}
+
+		public int getVersionCount() {
+			return versions.size();
+		}
+
+		public versionInfo getVersionInfo(int index) {
+			return versions.get(index);
+		}
+
+		public Object[] getArgs(){
+			return this.args;
+		}
+		
+	}
+	
+
+	
+}
